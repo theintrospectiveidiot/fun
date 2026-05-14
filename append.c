5f2a6df2947e9094a6a48aa36ca27b6e37ba8cad
theintrospectiveidiot <pbhaji57@gmail.com>
2026-05-14 20:59:37 +0530

    it works

--- append.c
+++ append.c
@@ -4,7 +4,7 @@ int main(int argc,char *argv[]) {
 	FILE *g = fopen(argv[2],"a+");
 	FILE *f = fopen(argv[1],"r+");
 	FILE *h = fopen("gen_counter.c","r");
-	char d,c;
+	char c,d;
 	fputc(60,g);
 	while((c = fgetc(f)) != 10) {
 		fputc(c,g);
