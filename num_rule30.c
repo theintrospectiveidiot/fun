4d2f7e06da64dcbdd8ad998183f494a3a31d99c4
theintrospectiveidiot <pbhaji57@gmail.com>
2026-07-08 15:04:41 +0530

    no change in main code

--- num_rule30.c
+++ num_rule30.c
@@ -10,16 +10,18 @@ int main() {
 		}
 	}
 	
-	A[0][252+1] = 1;
-	A[0][252+5] = 1;
-	A[0][252+7] = 1;
-	put_stuff("~d\n",bin_to_int(A[0] + 252,8));
+    //69 => 0 1 0 0 0 1 0 1
+
+	A[0][512 - 1] = 1;
+	A[0][512 - 3] = 1;
+	A[0][512 - 7] = 1;
+	put_stuff(1,"~d\n",bin_to_int(A[0], 512));
 	for(int i=1;i<p;i++) {
 		for(int j=1;j<2*p-1;j++) {
 			if(A[i-1][j] + A[i-1][j+1] == 0) {
 				A[i][j] = A[i-1][j-1];
 			}
-			else A[i][j] = comp(A[i-1][j-1]);
+			else A[i][j] = !(A[i-1][j-1]);
 		}	
 	}
 	
@@ -28,7 +30,7 @@ int main() {
 		/*for(int j=0;j<2*p;j++) {
 			put_stuff("~d",A[i][j]);
 		}*/
-		put_stuff("\t ~d\t~d\t~s",i,bin_to_int(A[i] + 252,8),((bin_to_int(A[i] + 252,8) % 42) == 0) ? "Yup":"Nope");
-		put_stuff("\n");
+		put_stuff(1,"\t ~d\t~d\t~s",i,bin_to_int(A[i], 512),((bin_to_int(A[i], 512) % 42) == 0) ? "Yup":"Nope");
+		put_stuff(1,"\n");
 	}
 }
